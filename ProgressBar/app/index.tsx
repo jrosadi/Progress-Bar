c37c9465825198da465a60455deb23acf96d9f66
J <julian.a.k.rosadi@gmail.com>
2025-09-16 11:44:38 -0500

    imported paper

--- ProgressBar/app/index.tsx
+++ ProgressBar/app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import MyProgressBar from "@/components/MyProgressBar";
+import { StyleSheet, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <MyProgressBar/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }
+})
